ed36bbede7f3c11f2022a9ad14bdc18088adba67
junwoo07 <junwoo07.seo@gmail.com>
2021-03-26 22:42:42 +0900

    초기 커밋.

---
+++ RandomPickPrject/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RandomPickPrject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            List<int> items = new List<int>(new int[]{1,2,3,4,5});
+            double down_speed_scale = 0.03 ;
+            double speed = 0.01;
+            int value ;
+            double endspeed = 0.6;
+            while (speed < endspeed)
+            {
+                value = items[random.Next(items.Count())];
+                speed += ((double)random.Next(-2,3))*down_speed_scale;
+                if (speed < 0)
+                {
+                    speed -= ((double)random.Next(-1, 3)) * down_speed_scale;
+                }
+                Console.WriteLine((int)(speed * 1000));
+                Thread.Sleep((int)(speed * 1000));
+                Console.Write("\r"+value.ToString());
+            }
+            Console.WriteLine("\n\n\n\n\nFinished!");
+        }
+    }
+}
